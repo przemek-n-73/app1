098c39c68a3088f2abbc2c7ab64a53384e26956c
Przemek N <przemyslaw.napieralski@gmail.com>
2023-08-08 08:49:40 +0200

    1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int myAge = 12;
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(myAge-12);
+uint qpa = 1998;
+Console.WriteLine("qpa = "+qpa); ;
+double zmiennoprzecinek = qpa / 13;
+Console.WriteLine(zmiennoprzecinek);
+string text1 = "Przemuś";
+string text2 = "Napieralski";
+string text3 = " ";
+Console.WriteLine(text1+text3+text2);
+char letter = 'Q';
+Console.WriteLine(letter);
+char letter2 = text1.ToArray()[1];
+Console.WriteLine(letter2);
