79bae83da93b07607a95c803f401673e15b405d1
Przemek N <przemyslaw.napieralski@gmail.com>
2023-09-06 09:02:39 +0200

    day6

---
+++ ConsoleApp1/ConsoleApp1/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    public class Employee
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+        public int Result
+        {
+            get
+            {
+                return score.Sum();
+            }
+        }
+
+        List<int> score = new List<int>();
+
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public void AddScore(int points)
+        {
+            this.score.Add(points);
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,18 +1,46 @@
-﻿int number = 4519566;
-string numberToString = number.ToString();
-char[] letters = numberToString.ToArray();
-// int[] numbers = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+﻿using ConsoleApp1;
 
-int[] numbers = new int[10]; 
+Employee employee1 = new Employee("Mieszko", "Pierwszy", 33);
+Employee employee2 = new Employee("Bolesław", "Drugi", 46);
+Employee employee3 = new Employee("Jan", "Trzeci", 27);
 
-foreach (char letter in letters)
+employee1.AddScore(15);
+employee1.AddScore(4);
+employee1.AddScore(29);
+employee1.AddScore(21);
+employee1.AddScore(2);
+
+employee2.AddScore(18);
+employee2.AddScore(1);
+employee2.AddScore(9);
+employee2.AddScore(26);
+employee2.AddScore(13);
+
+employee3.AddScore(6);
+employee3.AddScore(21);
+employee3.AddScore(16);
+employee3.AddScore(3);
+employee3.AddScore(17);
+
+Console.WriteLine(employee1.Name + " " + employee1.Surname + ": " + employee1.Result);
+Console.WriteLine(employee2.Name + " " + employee2.Surname + ": " + employee2.Result);
+Console.WriteLine(employee3.Name + " " + employee3.Surname + ": " + employee3.Result);
+
+List<Employee> employees = new List<Employee>()
 {
-    numbers[int.Parse(letter.ToString())] = numbers[int.Parse(letter.ToString())] + 1;
-}
+    employee1, employee2, employee3
+};
 
-Console.WriteLine(number+":");
-Console.WriteLine("");
-for (int index = 0; index < numbers.Length; index++)
+int theBestResult = -1;
+Employee theBestEmployee = default;
+
+foreach (Employee employee in employees)
 {
-    Console.WriteLine(index + " => " + numbers[index]);
+    if (employee.Result > theBestResult)
+    {
+        theBestResult = employee.Result;
+        theBestEmployee = employee;
+    }
 }
+Console.WriteLine("...czyli najlepszy jest:\n" + theBestEmployee.Name + " " + theBestEmployee.Surname + " z wynikiem: " + theBestEmployee.Result);
+
